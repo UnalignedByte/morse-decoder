6c0f99b9fb9ea5137bb4ae8f7abf43c91f26b340
Rafał Grodziński <rafal@unalignedbyte.com>
2020-02-17 00:40:42 +0900

    Sequence gets decoded

--- MorseDecoder/MorseDecoder.ino
+++ MorseDecoder/MorseDecoder.ino
@@ -1,3 +1,10 @@
+// Config
+const unsigned long DELAY_LONG_PRESS = 500;
+const unsigned long DELAY_DECODE = 500;
+const int BUTTON = 2;
+const int LED_SHORT = 3;
+const int LED_LONG = 4;
+
 typedef enum {
   dot,
   dash
@@ -10,14 +17,69 @@ typedef enum {
   released
 } State;
 
-const unsigned long DELAY_LONG_PRESS = 1000;
-const unsigned long DELAY_DECODE = 1000;
-const int BUTTON = 2;
-const int LED_SHORT = 3;
-const int LED_LONG = 4;
-const int CODES_MAX = 5;
+typedef struct {
+  char letter;
+  int hash;
+} MorseChar;
 
-char characterForSequence(Code *sequence, int length) {
+const int CODES_MAX = 5;
+const int MORSE_CHARS_COUNT = 36;
+
+MorseChar morseAlphabet[MORSE_CHARS_COUNT] = {
+  {'A', 21},
+  {'B', 1112},
+  {'C', 1212},
+  {'D', 112},
+  {'E', 1},
+  {'F', 1211},
+  {'G', 122},
+  {'H', 1111},
+  {'I', 11},
+  {'J', 2221},
+  {'K', 212},
+  {'L', 1121},
+  {'M', 22},
+  {'N', 12},
+  {'O', 222},
+  {'P', 1221},
+  {'Q', 2122},
+  {'R', 121},
+  {'S', 111},
+  {'T', 2},
+  {'U', 211},
+  {'V', 2111},
+  {'W', 221},
+  {'X', 2112},
+  {'Y', 2212},
+  {'Z', 1122},
+  {'1', 22221},
+  {'2', 22211},
+  {'3', 22111},
+  {'4', 21111},
+  {'5', 11111},
+  {'6', 11112},
+  {'7', 11122},
+  {'8', 11222},
+  {'9', 12222},
+  {'0', 22222}
+};
+
+int hashForSequence(Code *sequence, int length) {
+  int hash = 0;
+  int multiplier = 1;
+  for(int i=0; i<length; i++) {
+    int value = 1;
+    if(sequence[i] == dash) {
+      value = 2;
+    }
+    hash += value * multiplier;
+    multiplier *= 10;
+  }
+  return hash;
+}
+
+char characterForSequence(Code sequence[], int length) {
+  // Print print entered sequence
   for(int i=0; i<length; i++) {
     Serial.print(sequence[i] == dot ?  "dot" : "dash");
     if(i == (length - 1)) {
@@ -26,7 +88,18 @@ char characterForSequence(Code *sequence, int length) {
       Serial.print(", ");
     }
   }
-  return 'A';
+  // Encode the sequence
+  int hash = hashForSequence(sequence, length);
+  Serial.println(hash);
+  // Find letter correponding for the hash value
+  char decodedChar = '-';
+  for(int i=0; i<MORSE_CHARS_COUNT; i++) {
+    if(morseAlphabet[i].hash == hash) {
+      decodedChar = morseAlphabet[i].letter;
+      break;
+    }
+  }
+  return decodedChar;
 }
 
 State state = waiting;
@@ -35,7 +108,7 @@ Code currentCode;
 Code codes[CODES_MAX];
 int codeIndex = 0;
 
-void setup() {
+void setup() {  
   pinMode(BUTTON, INPUT);
   pinMode(LED_SHORT, OUTPUT);
   pinMode(LED_LONG, OUTPUT);
@@ -47,7 +120,7 @@ void loop() {
   long timeElapsed = millis() - startTime;
 
   // Press started
-  if((state == waiting || state == released) && buttonState == HIGH && codeIndex <= CODES_MAX) {
+  if((state == waiting || state == released) && buttonState == HIGH && codeIndex < CODES_MAX) {
     Serial.println("shortPressed");
     state = shortPressed;
     currentCode = dot;
